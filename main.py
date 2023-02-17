dc32eab171a5770fa71c552ebac378bb500e2141
flyash2 <125638409+flyash2@users.noreply.github.com>
2023-02-17 18:39:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def get_weather(region):
 
     return weather, temp, wind_dir,  air_quality, air_data
  
- def get_weather2(region):
+def get_weather2(region):
      headers = {
          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
