2122c0eea27aaf51b4084d2d3933530edae4ddf4
flyash2 <125638409+flyash2@users.noreply.github.com>
2023-02-17 20:06:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -72,12 +72,12 @@ def get_weather2(region):
      }
     key = config["weather_key"]
     region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, key)
-    response = get(region_url, headers=headers).json()
-    if response["code"] == "404":
+    response1 = get(region_url, headers=headers).json()
+    if response1["code"] == "404":
         print("推送消息失败，请检查地区名是否有误！")
         os.system("pause")
         sys.exit(1)
-    elif response["code"] == "401":
+    elif response1["code"] == "401":
         print("推送消息失败，请检查和风天气key是否正确！")
         os.system("pause")
         sys.exit(1)
@@ -85,11 +85,11 @@ def get_weather2(region):
          # 获取地区的location--id
          location_id = response["location"][0]["id"]
     weather_url = "https://devapi.qweather.com/v7/weather/daily?location={}&key={}".format(location_id, key)
-    response = get(weather_url, headers=headers).json()
+    response1 = get(weather_url, headers=headers).json()
     # 最高温度
-    max_temperature = response["daily"]["tempMax"] + u"\N{DEGREE SIGN}" + "C"
+    max_temperature = response1["daily"]["tempMax"] + u"\N{DEGREE SIGN}" + "C"
     # 最低温度
-    min_temperature = response["daily"]["tempMin"] + u"\N{DEGREE SIGN}" + "C"
+    min_temperature = response1["daily"]["tempMin"] + u"\N{DEGREE SIGN}" + "C"
 
 
 
