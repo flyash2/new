74b2e177618f7223c98fab0df496e16e1bfd52a0
flyash2 <125638409+flyash2@users.noreply.github.com>
2023-02-26 10:13:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from zhdate import ZhDate
 import sys
 import os
 
-
+啊实打实大
 def get_color():
     # 获取随机颜色
     get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
