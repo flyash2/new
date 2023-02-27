2511891dfc7d98df19deba7dbce8d743e7a236bb
flyash2 <125638409+flyash2@users.noreply.github.com>
2023-02-27 13:06:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         else:
             birthday_data = "{}".format(birth_day)
         # 将生日数据插入data
-        data["data"][key] = {"value": birthday_data, "color": get_color()}
+        data["data"][key] = {"value": birthday_data, "color": 0xFAEDAA}
     headers = {
         'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
